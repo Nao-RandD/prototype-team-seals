91003643c6de326cac27ce22e4b69fcb1be902a0
TAATHub <20purpleblue10@gmail.com>
2025-03-23 00:50:28 +0900

    temp

--- Camp/Camp/ViewModel/ImmersiveViewModel.swift
+++ Camp/Camp/ViewModel/ImmersiveViewModel.swift
@@ -7,13 +7,23 @@ import RealityKitContent
 final class ImmersiveViewModel {
     var rootEntity: Entity?
     
-    func setup(appModel: AppModel, environment: CampEnvironment) async {
+    func setup(appModel: AppModel, environment: CampEnvironment, crafts: [Craft]) async {
         rootEntity?.children.removeAll()
         
         // TODO: Switch scene name from environment
         guard let scene = try? await Entity(named: "Spring", in: realityKitContentBundle) else { return }
         scene.scale *= 10
         rootEntity?.addChild(scene)
+        
+        for craft in crafts {
+            if let entity = scene.findEntity(named: craft.name) {
+                var position = craft.translation.simd3
+                
+                entity.setPosition(position, relativeTo: nil)
+//                    entity.setScale(craft.scale.simd3, relativeTo: nil)
+                entity.setOrientation(.init(vector: craft.orientation.simd4), relativeTo: nil)
+            }
+        }
 
         if let skybox = Entity.createSkybox(environment: environment) {
             rootEntity?.addChild(skybox)
