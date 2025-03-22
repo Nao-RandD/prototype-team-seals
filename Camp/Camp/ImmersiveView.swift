1e7cd18b0801eb0f94c790ddaf3b220afc7e3d34
TAAT <taat@TAATnoMacBook-Pro.local>
2025-03-22 14:30:58 +0900

    Changed scene layout

--- Camp/Camp/ImmersiveView.swift
+++ Camp/Camp/ImmersiveView.swift
@@ -15,11 +15,15 @@ struct ImmersiveView: View {
         RealityView { content in
             // Add the initial RealityKit content
             if let immersiveContentEntity = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
+                immersiveContentEntity.scale *= 10
                 content.add(immersiveContentEntity)
 
                 // Put skybox here.  See example in World project available at
                 // https://developer.apple.com/
             }
+        }
+        .craftGesture { craft in
+            // TODO: Save craft info
         }
     }
 }
