3c280949fa760880dab01099d3065282908e0f45
TAAT <taat@TAATnoMacBook-Pro.local>
2025-03-22 12:04:43 +0900

    Added CraftGesture

---
+++ Camp/Camp/CraftGesture.swift
@@ -0,0 +1,114 @@
+import SwiftUI
+import RealityKit
+
+/// A gesture modifier handling drag, rotate, magnify gestures.
+struct CraftGesture: ViewModifier {
+
+    @State var isActive: Bool = false	
+    @State var initialPosition: SIMD3<Float> = .zero
+    @State var initialScale: SIMD3<Float> = .one
+    @State var initialOrientation:simd_quatf = simd_quatf(vector: .zero)
+
+    var onGestureEnded: (Craft) -> Void
+    
+    func body(content: Content) -> some View {
+        content
+            .gesture(dragGesture)
+            .gesture(rotateGesture)
+    }
+    
+    /// Drag gesture
+    ///
+    /// See also [Improved Drag Gesture](https://stepinto.vision/example-code/improved-drag-gesture/)
+    var dragGesture: some Gesture {
+        DragGesture()
+            .targetedToAnyEntity()
+            .onChanged { value in
+                startGesture(with: value.entity)
+                
+                // Update position from drag gesture
+                let movement = value.convert(value.gestureValue.translation3D, from: .local, to: .scene)
+                let newPosition = initialPosition + movement
+                value.entity.position = newPosition
+            }
+            .onEnded { value in
+                endGesture()
+                onGestureEnded(.init(entity: value.entity))
+            }
+    }
+    
+    var rotateGesture: some Gesture {
+        RotateGesture3D(constrainedToAxis: .y)
+            .targetedToAnyEntity()
+            .onChanged { value in
+                startGesture(with: value.entity)
+                
+                guard initialOrientation.length > 0 else { return }
+                
+                // Update rotation from rotate gesture
+                let rotationTransform = Transform(AffineTransform3D(rotation: value.rotation))
+                value.entity.orientation = initialOrientation * rotationTransform.rotation
+            }
+            .onEnded { value in
+                endGesture()
+                onGestureEnded(.init(entity: value.entity))
+            }
+    }
+    
+    /// Simultaneously combined rotate and magnify gesture
+    ///
+    /// See also [Simultaneously Combine Gestures](https://stepinto.vision/example-code/simultaneously-combine-gestures/)
+    var rotateAndMagnifyGesture: some Gesture {
+        RotateGesture3D(constrainedToAxis: .y)
+            .simultaneously(with: MagnifyGesture())
+            .targetedToAnyEntity()
+            .onChanged { value in
+                startGesture(with: value.entity)
+                
+                guard initialOrientation.length > 0 else { return }
+                
+                if let rotation = value.first?.rotation, let magnification = value.second?.magnification {
+                    // Update rotation from rotate gesture
+                    let rotationTransform = Transform(AffineTransform3D(rotation: rotation))
+                    value.entity.orientation = initialOrientation * rotationTransform.rotation
+                    
+                    // Update scale from magnify gesture
+                    let newScale = initialScale * Float(magnification)
+                    value.entity.scale = newScale
+                }
+
+            }
+            .onEnded { value in
+                endGesture()
+                onGestureEnded(.init(entity: value.entity))
+            }
+    }
+    
+    private func startGesture(with entity: Entity) {
+        guard !isActive else { return }
+        
+        isActive = true
+        
+        // Cache the entity's initial position, scale and rotation
+        initialPosition = entity.position
+        initialScale = entity.scale
+        initialOrientation = entity.transform.rotation
+    }
+    
+    private func endGesture() {
+        guard isActive else { return }
+        
+        isActive = false
+        
+        // Reset the entity's initial position, scale and rotation
+        initialPosition = .zero
+        initialScale = .one
+        initialOrientation = simd_quatf(vector: .zero)
+    }
+}
+
+extension View {
+    func craftGesture(onGestureEnded: @escaping (Craft) -> Void) -> some View {
+        modifier(CraftGesture(onGestureEnded: onGestureEnded))
+    }
+}
