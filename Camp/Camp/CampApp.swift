c41c6ce2c9786de16da3f018c2462f74c1c7d241
TAAT <taat@TAATnoMacBook-Pro.local>
2025-03-22 10:32:17 +0900

    Added initial project

---
+++ Camp/Camp/CampApp.swift
@@ -0,0 +1,33 @@
+//
+//  CampApp.swift
+//  Camp
+//
+//  Created by TAAT on 2025/03/22.
+//
+
+import SwiftUI
+
+@main
+struct CampApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
