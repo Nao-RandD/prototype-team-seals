4f9df43144fdb8cae68962c1b6fafa54d2df3c93
TAAT <taat@TAATnoMacBook-Pro.local>
2025-03-23 11:56:50 +0900

    Fixed position scale when restored

--- Camp/Camp/ViewModel/ImmersiveViewModel.swift
+++ Camp/Camp/ViewModel/ImmersiveViewModel.swift
@@ -5,18 +5,21 @@ import RealityKitContent
 @Observable
 @MainActor
 final class ImmersiveViewModel {
+    let sceneScale: Float = 10
+    
     var rootEntity: Entity?
     
     func setup(appModel: AppModel, environment: CampEnvironment, crafts: [Craft]) async {
         rootEntity?.children.removeAll()
         
         guard let scene = try? await Entity(named: environment.rawValue, in: realityKitContentBundle) else { return }
-        scene.scale *= 10
+        scene.scale *= sceneScale
         rootEntity?.addChild(scene)
         
         for craft in crafts {
             if let entity = scene.findEntity(named: craft.name) {
                 var position = craft.translation.simd3
+                position *= sceneScale
                 
                 entity.setPosition(position, relativeTo: nil)
 //                    entity.setScale(craft.scale.simd3, relativeTo: nil)
